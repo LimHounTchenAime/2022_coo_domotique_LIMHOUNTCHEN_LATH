17d94994778227321b0be4285c3567b23ffb9c9d
limhount1u <aime.lim-houn-tchen5@etu.univ-lorraine.fr>
2022-04-07 08:53:00 +0200

    Ajout Ajout classe Telecommande en intégralité

---
+++ src/Telecommande.java
@@ -0,0 +1,39 @@
+public class Telecommande {
+    //attributs
+    private Lampe[] lampes;
+
+    //constructeur
+    public Telecommande(){
+        lampes=new Lampe[0];
+    }
+
+    //methodes
+    public void ajouterLampe(Lampe lampe){
+        Lampe[] tmp=lampes;
+        lampes=new Lampe[lampe.length+1];
+        lampes[tmp.length]=new Lampe(lampe);
+        for(int i=0;i<tmp.length;i++)
+            lampes[i]=tmp[i];
+    }
+
+    public void activerLampe(int indiceLampe){
+        lampes[indiceLampe].allumer();
+    }
+
+    public void desactiverLampe(int indiceLampe){
+        lampes[indiceLampe].eteindre();
+    }
+
+    public void activerTout(){
+        for(int i=0;i<lampes.length;i++)
+            lampes[i].allumer();
+    }
+
+    public String toString(){
+        String res="";
+        for(int i=0;i<lampes.length;i++){
+            res+="Lampe "+i+" :"+lampes[i].allume();
+        }
+        return res;
+    }
+}
