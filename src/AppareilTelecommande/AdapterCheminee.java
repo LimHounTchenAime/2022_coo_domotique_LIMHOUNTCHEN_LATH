d6af6c33bf26aa0db4896b30086c4f539c9c6dd7
Castorsorcier <aimelimhountchen@gmail.com>
2022-04-29 17:01:09 +0200

    Ajout Classe Cheminee Classe AdapterCheminee Methode getNombre dans Telecommande

---
+++ src/AppareilTelecommande/AdapterCheminee.java
@@ -0,0 +1,9 @@
+package AppareilTelecommande;
+
+public class AdapterCheminee {
+    private Cheminee cheminee;
+
+    public AdapterCheminee(Cheminee cheminee2){
+        this.cheminee=cheminee2;
+    }
+}
--- src/AppareilTelecommande/Telecommande.java
+++ src/AppareilTelecommande/Telecommande.java
@@ -2,41 +2,45 @@ package AppareilTelecommande;
 
 import java.util.ArrayList;
 
-public class Telecommande{
+public class Telecommande {
     //attributs
     private ArrayList<Appareil> appareils;
 
     //constructeur
-    public Telecommande(){
-        appareils=new ArrayList<Appareil>();
+    public Telecommande() {
+        appareils = new ArrayList<Appareil>();
     }
 
     //methodes
-    public void ajouterAppareil(Appareil appareil){
+    public void ajouterAppareil(Appareil appareil) {
         appareils.add(appareil);
     }
 
-    public void activerAppareil(int indiceAppareil){
+    public void activerAppareil(int indiceAppareil) {
         appareils.get(indiceAppareil).allumer();
     }
 
-    public void desactiverAppareil(int indiceAppareil){
+    public void desactiverAppareil(int indiceAppareil) {
         appareils.get(indiceAppareil).eteindre();
     }
 
-    public void activerTout(){
-        for(int i=0;i<appareils.size();i++)
+    public void activerTout() {
+        for (int i = 0; i < appareils.size(); i++)
             appareils.get(i).allumer();
     }
 
-    public Appareil get(int i){
+    public Appareil get(int i) {
         return appareils.get(i);
     }
 
-    public String toString(){
-        String res="";
-        for(int i=0;i<appareils.size();i++){
-            res+="Appareil "+i+" :"+appareils.get(i).isAllume();
+    public int getNombre() {
+        return (this.appareils.size());
+    }
+
+    public String toString() {
+        String res = "";
+        for (int i = 0; i < appareils.size(); i++) {
+            res += "Appareil " + i + " :" + appareils.get(i).isAllume();
         }
         return res;
     }
