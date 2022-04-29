e524869f0e0ab1fae41ee4edb593bed88f76940c
Castorsorcier <aimelimhountchen@gmail.com>
2022-04-29 17:40:24 +0200

    Ajout Implements AdapterCheminee

--- src/AppareilTelecommande/AdapterCheminee.java
+++ src/AppareilTelecommande/AdapterCheminee.java
@@ -1,6 +1,6 @@
 package AppareilTelecommande;
 
-public class AdapterCheminee{
+public class AdapterCheminee implements Appareil{
     private Cheminee cheminee;
 
     public AdapterCheminee(Cheminee cheminee2){
