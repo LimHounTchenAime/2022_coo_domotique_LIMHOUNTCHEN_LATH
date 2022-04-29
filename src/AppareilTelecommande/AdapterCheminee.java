e5ce0545ba8e6111932c3b163ebc5f285c222d83
Castorsorcier <aimelimhountchen@gmail.com>
2022-04-29 17:31:01 +0200

    Ajout Méthode allumer classe AdapterCheminee Méthode eteindre classe AdapterCheminee

--- src/AppareilTelecommande/AdapterCheminee.java
+++ src/AppareilTelecommande/AdapterCheminee.java
@@ -6,4 +6,12 @@ public class AdapterCheminee{
     public AdapterCheminee(Cheminee cheminee2){
         this.cheminee=cheminee2;
     }
+
+    public void allumer(){
+        this.cheminee.changerIntensite(this.cheminee.getLumiere()+10);
+    }
+
+    public void eteindre(){
+        this.cheminee.changerIntensite(0);
+    }
 }
