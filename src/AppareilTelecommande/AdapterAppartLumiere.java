af5090f0c3a928267062723703b809d843f0d93f
lath3u <victor.lath9@etu.univ-lorraine.fr>
2022-05-02 09:11:50 +0200

    class AdapterAppartLumiere.java

--- src/AppareilTelecommande/AdapterAppartLumiere.java
+++ src/AppareilTelecommande/AdapterAppartLumiere.java
@@ -1,4 +1,32 @@
 package AppareilTelecommande;
 
-public class AdapterAppartLumiere {
+import appartement.AppareilAppart;
+import appartement.AppareilAppartLampe;
+
+public class AdapterAppartLumiere implements Appareil {
+
+    private AppareilAppartLampe adapter;
+
+
+    AdapterAppartLumiere(AppareilAppart a,String piece) {
+        this.adapter = (AppareilAppartLampe) a;
+    }
+
+    public void allumer() {
+        this.adapter.allumer();
+    }
+
+    public void eteindre() {
+        this.adapter.eteindre();
+    }
+
+    public boolean isAllume() {
+        boolean allume;
+        allume = this.adapter.toString().equals("On");
+        return  allume;
+    }
+
+    public String toString(){
+        return this.adapter.toString();
+    }
 }
