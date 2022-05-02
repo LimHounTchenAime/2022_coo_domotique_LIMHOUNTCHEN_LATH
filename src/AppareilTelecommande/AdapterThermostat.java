6e09461e55cbec2a535b228941029c967b27a147
lath3u <victor.lath9@etu.univ-lorraine.fr>
2022-05-02 08:13:22 +0200

    class AdapterThermostat

--- src/AppareilTelecommande/AdapterThermostat.java
+++ src/AppareilTelecommande/AdapterThermostat.java
@@ -4,13 +4,17 @@ import thermos.Thermostat;
 
 public class AdapterThermostat implements Appareil {
     private Thermostat thermostat;
+    private int temperature;
 
     public AdapterCheminee(Thermostat t) {
         this.thermostat = t;
+        this.temperature = 20;
     }
 
     public void allumer() {
-        this.thermostat.monterTemperature();
+        for (int i = 0; i<20;i++) {
+            this.thermostat.monterTemperature();
+        }
     }
 
     public void eteindre() {
@@ -22,5 +26,9 @@ public class AdapterThermostat implements Appareil {
     public boolean isAllume() {
         boolean allume;
         if
+    }
+
+    public String toString(){
+
     }
 }
